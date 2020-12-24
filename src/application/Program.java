019f2e968412d4f08a60c9a9f5bf529fad0c5f95
tales112 <tales.trab@gmail.com>
2020-12-24 01:50:09 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
@@ -13,6 +15,7 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
 		
+			SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 
 	}
